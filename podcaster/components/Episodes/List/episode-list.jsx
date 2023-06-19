4c5f25c0ef3d1d9db8d9119a5bce2f115fa437fb
German Bertea <gerbertea@gmail.com>
2023-06-19 19:22:19 +0200

    small fix for episodes

--- podcaster/components/Episodes/List/episode-list.jsx
+++ podcaster/components/Episodes/List/episode-list.jsx
@@ -1,4 +1,4 @@
-import { Table } from '@nextui-org/react';
+import { Table, Container } from '@nextui-org/react';
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import { millisToMinutesAndSeconds, transformDate } from '@/helpers/utils';
@@ -41,33 +41,36 @@ const EpisodesList = (props) => {
     router.push(`/podcast/${podcastId}/episode/${episodeId}`);
   };
   return (
-    <Table
-      aria-label='Episodes list'
-      color='primary'
-      bordered
-      sticked
-      selectionMode='single'
-      onRowAction={onSelectionChange}
-      css={{
-        minWidth: '60%',
-        maxWidth: '60%',
-        width: '60%',
-        height: 'auto',
-      }}
-    >
-      <Table.Header columns={columns}>
-        {(column) => (
-          <Table.Column key={column.key}>{column.label}</Table.Column>
-        )}
-      </Table.Header>
-      <Table.Body items={episodesLoaded}>
-        {(item) => (
-          <Table.Row key={item.key}>
-            {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
-          </Table.Row>
-        )}
-      </Table.Body>
-    </Table>
+    <Container lg gap={2} css={{ mt: '$10' }}>
+      <Table
+        aria-label='Episodes list'
+        color='primary'
+        bordered
+        sticked
+        selectionMode='single'
+        onRowAction={onSelectionChange}
+        css={{
+          minWidth: '60%',
+          maxWidth: '60%',
+          width: '60%',
+          height: 'auto',
+          cursor: 'pointer',
+        }}
+      >
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column key={column.key}>{column.label}</Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body items={episodesLoaded}>
+          {(item) => (
+            <Table.Row key={item.key}>
+              {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
+    </Container>
   );
 };
 
